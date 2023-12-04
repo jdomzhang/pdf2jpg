bddb95995226ad119e602a5d09f89226ad2c27fc
Steven Zhang <jdomzhang@gmail.com>
2023-12-05 01:06:30 +0800

    check args

---
+++ readme.md
@@ -0,0 +1,36 @@
+# pdf2jpg
+
+## pdfium
+
+- [binary files](https://github.com/bblanchon/pdfium-binaries/releases/tag/chromium%2F6164)
+
+## cross-compile
+
+### target
+
+```bash
+rustup target list
+rustup target add x86_64-unknown-linux-musl
+```
+
+### build on macos for linux
+
+```bash
+TARGET_CC=x86_64-linux-musl-gcc cargo build --release --target x86_64-unknown-linux-musl
+```
+
+## install on mac
+
+```bash
+sudo wget https://cdn.edesoft.com/tools/pdf2jpg/macos/pdf2jpg -O /usr/local/bin/pdf2jpg
+sudo chmod 777 /usr/local/bin/pdf2jpg
+sudo wget https://cdn.edesoft.com/tools/pdf2jpg/macos/libpdfium.dylib -O /usr/local/lib/libpdfium.dylib
+```
+
+## install on linux
+
+```bash
+sudo wget https://cdn.edesoft.com/tools/pdf2jpg/linux/pdf2jpg -O /usr/local/bin/pdf2jpg
+sudo chmod 777 /usr/local/bin/pdf2jpg
+sudo wget https://cdn.edesoft.com/tools/pdf2jpg/linux/libpdfium.so -O /usr/local/lib/libpdfium.so
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use image::ImageFormat;
 use pdfium_render::prelude::*;
 
@@ -19,6 +21,28 @@ pub fn main() -> Result<(), PdfiumError> {
         |_| Pdfium::bind_to_system_library(),
     )?;
 
+    // check args
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Usage: {} <pdf file> <output folder>", args[0]);
+        // return Ok(());
+        // return error
+        return Err(PdfiumError::UnrecognizedPath);
+    }
+
+    let pdf_file = &args[1];
+    let output_folder = &args[2];
+
+    // get file name without extension from pdf_file
+    let file_name = pdf_file.split("/").last().unwrap();
+    // remove extension
+    let file_name = file_name.split(".").next().unwrap();
+
+    // create folder if not existing
+    std::fs::create_dir_all(output_folder).unwrap();
+
+    // create pdfium
     let pdfium = Pdfium::new(bindings);
 
     // This pattern is common enough that it is the default constructor for the Pdfium struct,
@@ -30,14 +54,19 @@ pub fn main() -> Result<(), PdfiumError> {
     // sample file when rendering. Sharing the same rendering configuration is a good way
     // to ensure homogenous output across all pages in the document.
 
-    let render_config = PdfRenderConfig::new()
-        .set_target_width(2000)
-        .set_maximum_height(2000)
-        .rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
+    // let render_config = PdfRenderConfig::new()
+    //     // .set_target_width(2000)
+    //     // .set_target_width(1920 * 2);
+    //     .set_target_width(1920);
+    // // .set_maximum_width(1920 * 2)
+    // // .set_maximum_height(1920 * 2);
+    // // .set_maximum_height(2000);
+    // // .rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
 
     // Load the sample file...
 
-    let document = pdfium.load_pdf_from_file("test/export-test.pdf", None)?;
+    // let document = pdfium.load_pdf_from_file("test/export-test.pdf", None)?;
+    let document = pdfium.load_pdf_from_file(pdf_file, None)?;
 
     // ... and export each page to a JPEG in the current working directory,
     // using the rendering configuration we created above.
@@ -47,13 +76,22 @@ pub fn main() -> Result<(), PdfiumError> {
         .iter() // ... get an iterator across all pages ...
         .enumerate()
     {
+        let render_config = PdfRenderConfig::new().set_target_width(1920);
+        // .set_target_height(page.height().value as i32)
+        // .set_target_width(page.width().value as i32);
+
+        let page_file = format!("{}/{}-{}.jpg", output_folder, file_name, index + 1);
+        println!("{}", page_file);
+
         let result = page
             .render_with_config(&render_config)? // Initializes a bitmap with the given configuration for this page ...
             .as_image() // ... renders it to an Image::DynamicImage ...
             .as_rgba8() // ... sets the correct color space ...
             .ok_or(PdfiumError::ImageError)?
             .save_with_format(
-                format!("./output/export-test-page-{}.jpg", index + 1),
+                page_file,
+                // format!("./output/export-test-page-{}.jpg", index + 1),
+                // format!("{}/{}-{}.jpg", output_folder, file_name, index + 1),
                 ImageFormat::Jpeg,
             ); // ... and exports it to a JPEG.
 
